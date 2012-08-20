342bebc448bde3b7f04b9ea2294ca9426eb62f4f
Carlo <carlo.scarioni@gmail.com>
2012-08-20 18:20:53 +0200

    Update lib/with_gc_stats/core.rb

--- lib/with_gc_stats/core.rb
+++ lib/with_gc_stats/core.rb
@@ -11,6 +11,7 @@ class Object
   def gc_ruby_1_8(out)
     raise "Ruby GC options not supported in Ruby 1.8 unless you install the patch at https://github.com/patshaughnessy/ruby187gc, or install Ruby EE" unless GC.respond_to? :enable_stats
     GC.enable_stats
+    GC.clear_stats
     out.puts "How many GC Collections before running the code #{GC.collections}"
     out.puts "How many live objects we have before running the code #{ObjectSpace.live_objects}"
     allocated_before = ObjectSpace.allocated_objects
@@ -19,6 +20,7 @@ class Object
     out.puts "How many live objects we have after running the code #{ObjectSpace.live_objects}"
     out.puts "Time spent in GC, in miliseconds #{GC.time / 1000}"
     out.puts "Total allocated objects by the code #{ObjectSpace.allocated_objects - allocated_before}"
+    GC.disable_stats
   end
 
   def gc_ruby_1_9(out)
